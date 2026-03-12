4c323af7902283146db42e6bca6b58e768b7de8a
SHUBHAM KANERE <shubhamkanere24@gmail.com>
2026-03-12 16:54:17 +0530

    login system tested and modified

--- BP-Tracker/src/Services/api.jsx
+++ BP-Tracker/src/Services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bp-tracker-rct6.onrender.com/"
+  baseURL: "https://bp-tracker-rct6.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
